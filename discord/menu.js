0ad57754e4a123a3770c6a5cb4b91f6f68b66083
Tzero Ocne <12205169+TzeroOcne@users.noreply.github.com>
2023-07-21 21:45:20 +0700

    info: change script version

--- discord/menu.js
+++ discord/menu.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Discord Menu
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://discord.com/channels/558322816416743459/732948529030365184
