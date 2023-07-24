7e9781a8db0b8fc0d4b91e6651b21b3a70633384
Tzero Ocne <12205169+TzeroOcne@users.noreply.github.com>
2023-07-24 23:28:18 +0700

    wip: add font

--- discord/menu.js
+++ discord/menu.js
@@ -16,6 +16,13 @@ window.waitForSelector = waitForSelector;
 /** @type {(string) => Promise<HTMLElement>} */
 const waitForElem = waitForSelector;
 
+const leftIconStr = `
+<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+  <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+  <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160zm352-160l-160 160c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L301.3 256 438.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0z"/>
+</svg>
+`;
+
 (function() {
     'use strict';
 
@@ -26,10 +33,20 @@ const waitForElem = waitForSelector;
 $(async () => {
   console.log('Monkey here');
 
+  /** @type {HTMLScriptElement} */
+  // const fontAwesomeScript = document.createElement('script');
+  // fontAwesomeScript.src = 'https://kit.fontawesome.com/354b05a8cb.js';
+  // fontAwesomeScript.crossOrigin = 'anonymous';
+  // document.head.appendChild(fontAwesomeScript);
+
   const sidebar = await waitForElem('[class*="sidebar"]');
 
   const hideSidebarButton = document.createElement('div');
   hideSidebarButton.id = "nnryscript";
+  hideSidebarButton.innerHTML = leftIconStr;
   sidebar.appendChild(hideSidebarButton);
-});
 
+  // const fontAwesomeElement = document.createElement('i');
+  // fontAwesomeElement.classList.add('fa-solid', 'fa-thumbs-up', 'fa-5x');
+  // hideSidebarButton.appendChild(fontAwesomeElement);
+});
